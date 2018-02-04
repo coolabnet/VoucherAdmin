329556e9ca7c0db201b3dec1a97e34efbb6ff063
Hiure <hiure@riseup.net>
2018-02-04 10:16:20 -0200

    Update remvoucher.sh

--- remvoucher.sh
+++ remvoucher.sh
@@ -1,3 +1,21 @@
-grep -v "$1" db.csv > /tmp/$$
+
+scp root@thisnode.info:/etc/nodogsplash/vale/db.csv ./
+
+echo " ___________________________________________________ "
+echo "|                                                   |"
+echo "|                                                   |"
+echo "| Programa para remover usuarios, para isso digite  |"
+echo "| exatamente o nome como no cadastro                |"
+echo "|                                                   |"
+echo "|___________________________________________________|"
+
+printf "Nome do user a remover: "
+read nome
+
+grep -v "$nome" db.csv > /tmp/$$
 mv /tmp/$$ db.csv
 scp ./db.csv root@thisnode.info:/etc/nodogsplash/vale/db.csv
+cat db.csv | cut -f1 -d "," > user
+echo " "
+echo "Os usuarios cadastrados são:"
+cat user
