40cc90b4a1b07729488f2e35cefbadc050b3e845
Hiure <hiure@riseup.net>
2018-02-05 08:10:42 -0200

    Update listvoucher.sh

--- listvoucher.sh
+++ listvoucher.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 scp root@thisnode.info:/etc/nodogsplash/vale/db.csv ./
-echo Eu tenho `cat db.csv | wc -l` vouchers cadastrados
+echo Temos `cat db.csv | wc -l` vouchers cadastrados
 echo "Que São:"
 sort -o db.csv db.csv
 cat db.csv
