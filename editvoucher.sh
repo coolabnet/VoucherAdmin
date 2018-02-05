4a7cb7359f6a5a8c98edc6f3a376eee3c130f523
Hiure <hiure@riseup.net>
2018-02-05 08:07:24 -0200

    Add files via upload

--- editvoucher.sh
+++ editvoucher.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 scp root@thisnode.info:/etc/nodogsplash/vale/db.csv ./
 
 echo " ___________________________________________________ "
@@ -11,17 +12,22 @@ echo "|___________________________________________________|"
 printf "Nome do user para editar: "
 read nome
 
-
-
+if [ ${#nome}=0 ]
+then
 grep -v "$nome" db.csv > /tmp/$$
 mv /tmp/$$ db.csv
 
 printf "Digite um voucher de 12 digitos: "
 read voucher
-
-echo "$nome,$voucher,," >> db.csv
-sort -o db.csv db.csv
-
-scp ./db.csv root@thisnode.info:/etc/nodogsplash/vale/db.csv
-
-
+	if [ ${#voucher} -ne 12 ]
+	then
+	echo "O voucher que vc digitou tem ${#voucher}, e ele precisa ter 12 digitos"
+	else
+	
+	echo "$nome,$voucher,," >> db.csv
+        sort -o db.csv db.csv
+        scp ./db.csv root@thisnode.info:/etc/nodogsplash/vale/db.csv
+	fi
+else
+echo "Não deixe o nome em branco, isso atrapalha a vida!"
+fi
